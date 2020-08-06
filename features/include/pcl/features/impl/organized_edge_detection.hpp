AgileRobotsAG/pcl

commit 7781e112657c2ace45025e7a73240086e9f679d6

organized_edge_detection.hpp: Using algorithms and Eigen functions for better code

features/include/pcl/features/impl/organized_edge_detection.hpp
@@ -128,11 +128,11 @@ pcl::OrganizedEdgeBase<PointT, PointLT>::extractEdges (pcl::PointCloud<PointLT>&
         if (!found_invalid_neighbor)
         {
           // Every neighboring points are valid
-          std::vector<float>::iterator min_itr = std::min_element (nghr_dist.begin (), nghr_dist.end ());
-          std::vector<float>::iterator max_itr = std::max_element (nghr_dist.begin (), nghr_dist.end ());
+          std::vector<float>::const_iterator min_itr, max_itr;
+          std::tie (min_itr, max_itr) = std::minmax_element (nghr_dist.cbegin (), nghr_dist.cend ());
           float nghr_dist_min = *min_itr;
           float nghr_dist_max = *max_itr;
-          float dist_dominant = std::abs (nghr_dist_min) > std::abs (nghr_dist_max) ? nghr_dist_min : nghr_dist_max;
+          float dist_dominant = std::max(std::abs (nghr_dist_min),std::abs (nghr_dist_max));
           if (std::abs (dist_dominant) > th_depth_discon_*std::abs (curr_depth))
           {
             // Found a depth discontinuity

commit d65fdea32041a8cf2f58930987d98ce495985204

Fix logic bug in Organized Edge Detection
A bug was introduced in commit 7781e112657c2ace45025e7a73240086e9f679d6
that inadvertantly changed behavior of edge feature extraction for
occluding and occluded edges.  This commit reverts to previous logic

features/include/pcl/features/impl/organized_edge_detection.hpp
@@ -131,7 +131,7 @@ pcl::OrganizedEdgeBase<PointT, PointLT>::extractEdges (pcl::PointCloud<PointLT>&
           std::tie (min_itr, max_itr) = std::minmax_element (nghr_dist.cbegin (), nghr_dist.cend ());
           float nghr_dist_min = *min_itr;
           float nghr_dist_max = *max_itr;
-          float dist_dominant = std::max(std::abs (nghr_dist_min),std::abs (nghr_dist_max));
+          float dist_dominant = std::abs (nghr_dist_min) > std::abs (nghr_dist_max) ? nghr_dist_min : nghr_dist_max;
           if (std::abs (dist_dominant) > th_depth_discon_*std::abs (curr_depth))
           {
             // Found a depth discontinuity