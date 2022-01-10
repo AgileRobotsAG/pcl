AgileRobotsAG/pcl

commit 62ca35f51673fcecd7be9bb131c99b1019fc38af

Fixed `PCL_WARNINGS_ARE_ERRORS` option and some compile warnings (#4695)
* fixed PCL_WARNINGS_ARE_ERRORS option in CMakeLists

* fixed -Werror=class-memaccess in pcl/common/include/pcl/common/impl/io.hpp

fixed compile error like this,  when using -Werror:
pcl/common/include/pcl/common/impl/io.hpp:139:12: error: 'void* memcpy(void*, const void*, size_t)' copying an object of non-trivial type 'struct pcl::PointNormal' from an array of 'const struct pcl::PointXYZ' [-Werror=class-memaccess]

* fixed some memset on non-POD types

* fixed new errors

* restored 3rd opennurbs modification

* added compile flags for 3rd party & fixed deprecated copy

* fixed errors & set copy functions to default

* Update surface/include/pcl/surface/impl/convex_hull.hpp

fixed redundancy code with already defined data type

Co-authored-by: Kunal Tyagi <tyagi.kunal@live.com>

* fixed hull size in convex_hull.hpp

* added type AlignedVector for pcl/types.h

* fixed include library of Eigen::aligned_allocator

* fixed noexcept-type errors & added exclusive rules in CMakeLists

* resolved conflicts with master

* added inline comments

* added rules and fixed codes for compile on GPU

* made unsigned declaration more explict

* fixed noexcept-type error in test_common.cpp

* Moved for-loop into empty templated function

* Moved copyPointCloudMemcpy into detail namespace

Co-authored-by: Kunal Tyagi <tyagi.kunal@live.com>

features/include/pcl/features/impl/integral_image2D.hpp
@@ -158,7 +158,7 @@ IntegralImage2D<DataType, Dimension>::computeIntegralImages (
 {
   ElementType* previous_row = &first_order_integral_image_[0];
   ElementType* current_row  = previous_row + (width_ + 1);
-  memset (previous_row, 0, sizeof (ElementType) * (width_ + 1));
+  *previous_row = ElementType::Zero(width_ + 1);
 
   unsigned* count_previous_row = &finite_values_integral_image_[0];
   unsigned* count_current_row  = count_previous_row + (width_ + 1);
@@ -189,7 +189,7 @@ IntegralImage2D<DataType, Dimension>::computeIntegralImages (
   {
     SecondOrderType* so_previous_row = &second_order_integral_image_[0];
     SecondOrderType* so_current_row  = so_previous_row + (width_ + 1);
-    memset (so_previous_row, 0, sizeof (SecondOrderType) * (width_ + 1));
+    *so_previous_row = SecondOrderType::Zero(width_ + 1);
 
     SecondOrderType so_element;
     for (unsigned rowIdx = 0; rowIdx < height_; ++rowIdx, data += row_stride,

CMakeLists.txt
@@ -105,9 +105,10 @@ if(CMAKE_COMPILER_IS_GNUCXX)
       string(APPEND CMAKE_CXX_FLAGS " -Wabi")
     endif()
     string(APPEND CMAKE_CXX_FLAGS " -Wall -Wextra -Wno-unknown-pragmas -fno-strict-aliasing -Wno-format-extra-args -Wno-sign-compare -Wno-invalid-offsetof -Wno-conversion ${SSE_FLAGS}")
-    if(PCL_WARNINGS_ARE_ERRORS)
-      string(APPEND CMAKE_CXX_FLAGS " -Werror")
-    endif()
+  endif()
+
+  if(PCL_WARNINGS_ARE_ERRORS)
+    string(APPEND CMAKE_CXX_FLAGS " -Werror -fno-strict-aliasing")
   endif()
 
   if("${CMAKE_SHARED_LINKER_FLAGS}" STREQUAL "" AND NOT CMAKE_SYSTEM_NAME STREQUAL "Darwin")

apps/in_hand_scanner/include/pcl/apps/in_hand_scanner/impl/common_types.hpp
@@ -99,6 +99,8 @@ namespace pcl
         this->directions = other.directions;
       }
 
+      inline PointIHS& operator=(const PointIHS& other) = default;
+
       inline PointIHS (const pcl::PointXYZRGBNormal& other, const float weight)
       {
         this->x       = other.x;

apps/point_cloud_editor/include/pcl/apps/point_cloud_editor/selection.h
@@ -60,6 +60,11 @@ class Selection : public Statistics
         registerStats();
     }
 
+    /// @brief Copy constructor.
+    /// @param selection a const reference to a selection object whose
+    /// properties will be copied.
+    Selection (const Selection& selection) = default;
+
     /// @brief Equal operator
     /// @param selection a const reference to a selection object whose
     /// properties will be copied.

common/include/pcl/common/impl/io.hpp
@@ -117,6 +117,28 @@ getFieldsList (const pcl::PointCloud<PointT> &)
   return (result);
 }
 
+namespace detail
+{
+
+  template <typename PointInT, typename PointOutT> void
+  copyPointCloudMemcpy (const pcl::PointCloud<PointInT> &cloud_in,
+                        pcl::PointCloud<PointOutT> &cloud_out)
+  {
+    // Iterate over each point, if the point types of two clouds are different
+    for (std::size_t i = 0; i < cloud_in.size (); ++i)
+      copyPoint (cloud_in[i], cloud_out[i]);
+  }
+
+
+  template <typename PointT> void
+  copyPointCloudMemcpy (const pcl::PointCloud<PointT> &cloud_in,
+                        pcl::PointCloud<PointT> &cloud_out)
+  {
+    // Use std::copy directly, if the point types of two clouds are same
+    std::copy (&cloud_in[0], (&cloud_in[0]) + cloud_in.size (), &cloud_out[0]);
+  }
+
+} // namespace detail
 
 template <typename PointInT, typename PointOutT> void
 copyPointCloud (const pcl::PointCloud<PointInT> &cloud_in,
@@ -131,16 +153,8 @@ copyPointCloud (const pcl::PointCloud<PointInT> &cloud_in,
   cloud_out.sensor_origin_ = cloud_in.sensor_origin_;
   cloud_out.resize (cloud_in.size ());
 
-  if (cloud_in.empty ())
-    return;
-
-  if (isSamePointType<PointInT, PointOutT> ())
-    // Copy the whole memory block
-    memcpy (&cloud_out[0], &cloud_in[0], cloud_in.size () * sizeof (PointInT));
-  else
-    // Iterate over each point
-    for (std::size_t i = 0; i < cloud_in.size (); ++i)
-      copyPoint (cloud_in[i], cloud_out[i]);
+  if (!cloud_in.empty ())
+    detail::copyPointCloudMemcpy (cloud_in, cloud_out);
 }
 
 

common/include/pcl/types.h
@@ -49,6 +49,8 @@
 
 #include <cstdint>
 
+#include <Eigen/Core>
+
 namespace pcl
 {
   namespace detail {
@@ -129,5 +131,11 @@ namespace pcl
    * \brief Type used for indices in PCL
    */
   using Indices = IndicesAllocator<>;
+
+  /**
+   * \brief Type used for aligned vector of Eigen objects in PCL
+   */
+  template <typename T>
+  using AlignedVector = std::vector<T, Eigen::aligned_allocator<T>>;
 }  // namespace pcl
 

common/src/parse.cpp
@@ -181,14 +181,20 @@ parse_argument (int argc, const char * const * argv, const char * str, T &val) n
 int
 pcl::console::parse_argument (int argc, const char * const * argv, const char * str, double &val)
 {
-  return parse_generic(strtod, argc, argv, str, val);
+  // added lambda wrapper for `strtod` to handle noexcept-type warning in GCC 7,
+  // refer to: https://stackoverflow.com/questions/46798456/handling-gccs-noexcept-type-warning
+  const auto strtod_l = [](const char *str, char **str_end){ return strtod(str, str_end); };
+  return parse_generic(strtod_l, argc, argv, str, val);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 int
 pcl::console::parse_argument (int argc, const char * const * argv, const char * str, float &val)
 {
-  return parse_generic(strtof, argc, argv, str, val);
+  // added lambda wrapper for `strtof` to handle noexcept-type warning in GCC 7,
+  // refer to: https://stackoverflow.com/questions/46798456/handling-gccs-noexcept-type-warning
+  const auto strtof_l = [](const char *str, char **str_end){ return strtof(str, str_end); };
+  return parse_generic(strtof_l, argc, argv, str, val);
 }
 
 ////////////////////////////////////////////////////////////////////////////////

cuda/CMakeLists.txt
@@ -11,7 +11,9 @@ endif()
 if(CMAKE_COMPILER_IS_GNUCXX)
   string(REPLACE "-Wold-style-cast" "" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
   string(REPLACE "-Wno-invalid-offsetof" "" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-conversion -Wno-unused-parameter -Wno-unused-variable -Wno-unused-function -Wno-unused-but-set-variable")
+  string(APPEND CMAKE_CXX_FLAGS " -Wno-conversion -Wno-unused-parameter -Wno-unused-variable -Wno-unused-function -Wno-unused-but-set-variable")
+  # allow deprecation warnings in Eigen(3.3.7)/Core, see here: https://gitlab.kitware.com/vtk/vtk/-/issues/17661
+  string(APPEND CMAKE_CXX_FLAGS " -Wno-error=cpp")
 endif()
 
 collect_subproject_directory_names("${CMAKE_CURRENT_SOURCE_DIR}" "CMakeLists.txt" PCL_CUDA_MODULES_NAMES PCL_CUDA_MODULES_DIRS)

commit c3dd2457494a01c4c17a15b8621288affb4ddb28

Fix row initialization in integral image Problem introduced in commit https://github.com/PointCloudLibrary/pcl/commit/62ca35f51673fcecd7be9bb131c99b1019fc38af The function Eigen::Matrix::Zero(Index size) is intended only for dynamic-size vectors. If used for fixed-size vectors, size must be the vector length, else an assert in Eigen aborts the program. The row is correctly initialized by calling setZero() on every element.

features/include/pcl/features/impl/integral_image2D.hpp
@@ -158,7 +158,8 @@ IntegralImage2D<DataType, Dimension>::computeIntegralImages (
 {
   ElementType* previous_row = &first_order_integral_image_[0];
   ElementType* current_row  = previous_row + (width_ + 1);
-  *previous_row = ElementType::Zero(width_ + 1);
+  for (unsigned int i = 0; i < (width_ + 1); ++i)
+    previous_row[i].setZero();
 
   unsigned* count_previous_row = &finite_values_integral_image_[0];
   unsigned* count_current_row  = count_previous_row + (width_ + 1);
@@ -189,7 +190,8 @@ IntegralImage2D<DataType, Dimension>::computeIntegralImages (
   {
     SecondOrderType* so_previous_row = &second_order_integral_image_[0];
     SecondOrderType* so_current_row  = so_previous_row + (width_ + 1);
-    *so_previous_row = SecondOrderType::Zero(width_ + 1);
+    for (unsigned int i = 0; i < (width_ + 1); ++i)
+      so_previous_row[i].setZero();
 
     SecondOrderType so_element;
     for (unsigned rowIdx = 0; rowIdx < height_; ++rowIdx, data += row_stride,