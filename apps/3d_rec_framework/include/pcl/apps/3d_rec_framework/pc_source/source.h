AgileRobotsAG/pcl

commit dce3680b6b8acf665986d1449f7459388e5d7922

Convert to range-based for loops (#5417)
* Changed iterators to range-based for loops in apps

* Reverted construct

* Reverted another pseudo-container

* Removed constness

* Reverted a file I can't build/test locally

* Fixed a typo

* Applied another fix

* Modernized loop properly

* Fixed clang-format complaint

* Converted more for-loops to range based

* Fixed typo

* FIxed missed changes

* Changed (some) manual for loops to range-based

* Fixed clang-format escapes

* Rebased on upstream, fixed compile issues

* Reverted indexed loop conversions per review

* Made corrections per review

apps/3d_rec_framework/include/pcl/apps/3d_rec_framework/pc_source/source.h
@@ -194,16 +194,10 @@ public:
   std::shared_ptr<std::vector<ModelT>>
   getModels(std::string& model_id)
   {
-
-    typename std::vector<ModelT>::iterator it = models_->begin();
-    while (it != models_->end()) {
-      if (model_id != (*it).id_) {
-        it = models_->erase(it);
-      }
-      else {
-        it++;
-      }
-    }
+    models_->erase(std::remove_if(models_->begin(),
+                                  models_->end(),
+                                  [=](ModelT& s) { return (s.id_ == model_id); }),
+                   models_->end());
 
     return models_;
   }

apps/3d_rec_framework/include/pcl/apps/3d_rec_framework/utils/vtk_model_sampling.h
@@ -68,8 +68,7 @@ randPSurface(vtkPolyData* polydata,
 {
   float r = static_cast<float>(uniform_deviate(rand()) * totalArea);
 
-  std::vector<double>::iterator low =
-      std::lower_bound(cumulativeAreas->begin(), cumulativeAreas->end(), r);
+  auto low = std::lower_bound(cumulativeAreas->begin(), cumulativeAreas->end(), r);
   vtkIdType el = static_cast<vtkIdType>(low - cumulativeAreas->begin());
 
   double A[3], B[3], C[3];

apps/cloud_composer/src/commands.cpp
@@ -180,13 +180,12 @@ pcl::cloud_composer::CloudCommand::restoreOriginalRemoveNew (const QList <const 
     QList <QStandardItem*> removed = parent_item->takeRow (to_remove_index.row ());
   }
   //Restore the original items
-  QMap <QStandardItem*, QStandardItem*>::iterator itr;
   foreach (const CloudComposerItem* item, originals)
   {
     //Point iterator to the correct spot
     // Find doesn't modify parameter so it should accept a const pointer, but it can't be because it is templated to the map type
     // So we hack to get around this with a const cast
-    itr = removed_to_parent_map_.find (const_cast<CloudComposerItem*> (item));
+    const auto& itr = removed_to_parent_map_.find (const_cast<CloudComposerItem*> (item));
     QStandardItem* parent = itr.value ();
     QStandardItem* original = itr.key ();
     parent->appendRow (original);

apps/cloud_composer/src/project_model.cpp
@@ -610,7 +610,7 @@ pcl::cloud_composer::ProjectModel::onlyCloudItemsSelected ()
 void 
 pcl::cloud_composer::ProjectModel::setSelectedStyle (interactor_styles::INTERACTOR_STYLES style)
 {
-  QMap<interactor_styles::INTERACTOR_STYLES, bool>::iterator itr = selected_style_map_.begin();
+  auto itr = selected_style_map_.begin();
   while (itr != selected_style_map_.end ())
   {
     itr.value() = false;

apps/cloud_composer/tools/euclidean_clustering.cpp
@@ -69,13 +69,13 @@ pcl::cloud_composer::EuclideanClusteringTool::performAction (ConstItemList input
       qDebug () << "Found "<<cluster_indices.size ()<<" clusters!";
       int cluster_count = 0;
       pcl::ExtractIndices<pcl::PCLPointCloud2> filter;
-      for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin (); it != cluster_indices.end (); ++it)
+      for (const auto& cluster : cluster_indices)
       {
         filter.setInputCloud (input_cloud);
         // It's annoying that I have to do this, but Euclidean returns a PointIndices struct
-        pcl::PointIndices::ConstPtr indices_ptr = pcl::make_shared<pcl::PointIndices>(*it);
+        pcl::PointIndices::ConstPtr indices_ptr = pcl::make_shared<pcl::PointIndices>(cluster);
         filter.setIndices (indices_ptr);
-        extracted_indices->insert (extracted_indices->end (), it->indices.begin (), it->indices.end ());
+        extracted_indices->insert (extracted_indices->end (), cluster.indices.begin (), cluster.indices.end ());
         //This means remove the other points
         filter.setKeepOrganized (false);
         pcl::PCLPointCloud2::Ptr cloud_filtered (new pcl::PCLPointCloud2);

apps/in_hand_scanner/src/opengl_viewer.cpp
@@ -949,11 +949,9 @@ pcl::ihs::OpenGLViewer::drawMeshes()
     exit(EXIT_FAILURE);
   }
 
-  for (FaceVertexMeshMap::const_iterator it = drawn_meshes_.begin();
-       it != drawn_meshes_.end();
-       ++it) {
-    if (it->second && !it->second->vertices.empty()) {
-      const FaceVertexMesh& mesh = *it->second;
+  for (const auto& drawn_mesh : drawn_meshes_) {
+    if (drawn_mesh.second && !drawn_mesh.second->vertices.empty()) {
+      const FaceVertexMesh& mesh = *drawn_mesh.second;
 
       glVertexPointer(3, GL_FLOAT, sizeof(PointIHS), &(mesh.vertices[0].x));
       glNormalPointer(GL_FLOAT, sizeof(PointIHS), &(mesh.vertices[0].normal_x));

apps/modeler/src/scene_tree.cpp
@@ -173,15 +173,13 @@ pcl::modeler::SceneTree::slotOpenPointCloud()
     closePointCloud(cloud_mesh_items);
   }
 
-  for (QStringList::const_iterator filenames_it = filenames.begin();
-       filenames_it != filenames.end();
-       ++filenames_it) {
-    if (!openPointCloud(*filenames_it))
+  for (const auto& filename : filenames) {
+    if (!openPointCloud(filename))
       QMessageBox::warning(main_window,
                            tr("Failed to Open Point Cloud"),
                            tr("Can not open point cloud file %1, please check if it's "
                               "in valid .pcd format!")
-                               .arg(*filenames_it));
+                               .arg(filename));
   }
 }
 
@@ -206,14 +204,13 @@ pcl::modeler::SceneTree::slotImportPointCloud()
   if (filenames.isEmpty())
     return;
 
-  for (QStringList::const_iterator filenames_it = filenames.begin();
-       filenames_it != filenames.end();
-       ++filenames_it) {
-    if (!openPointCloud(*filenames_it))
+  for (const auto& filename : filenames) {
+    if (!openPointCloud(filename)) {
       QMessageBox::warning(
           main_window,
           tr("Failed to Import Point Cloud"),
-          tr("Can not import point cloud file %1 as .pcd file!").arg(*filenames_it));
+          tr("Can not import point cloud file %1 as .pcd file!").arg(filename));
+    }
   }
 }
 
@@ -257,11 +254,8 @@ pcl::modeler::SceneTree::closePointCloud(const QList<CloudMeshItem*>& items)
     delete item;
   }
 
-  for (QList<RenderWindowItem*>::const_iterator render_window_items_it =
-           render_window_items.begin();
-       render_window_items_it != render_window_items.end();
-       ++render_window_items_it) {
-    (*render_window_items_it)->getRenderWindow()->render();
+  for (const auto& render_window_item : render_window_items) {
+    render_window_item->getRenderWindow()->render();
   }
 }
 
@@ -381,10 +375,8 @@ pcl::modeler::SceneTree::slotUpdateOnSelectionChange(const QItemSelection& selec
                                                      const QItemSelection& deselected)
 {
   QModelIndexList selected_indices = selected.indexes();
-  for (QModelIndexList::const_iterator selected_indices_it = selected_indices.begin();
-       selected_indices_it != selected_indices.end();
-       ++selected_indices_it) {
-    QTreeWidgetItem* item = itemFromIndex(*selected_indices_it);
+  for (const auto& selected_index : selected_indices) {
+    QTreeWidgetItem* item = itemFromIndex(selected_index);
     RenderWindowItem* render_window_item = dynamic_cast<RenderWindowItem*>(item);
     if (render_window_item != nullptr) {
       render_window_item->getRenderWindow()->setActive(true);
@@ -392,12 +384,9 @@ pcl::modeler::SceneTree::slotUpdateOnSelectionChange(const QItemSelection& selec
   }
 
   QModelIndexList deselected_indices = deselected.indexes();
-  for (QModelIndexList::const_iterator deselected_indices_it =
-           deselected_indices.begin();
-       deselected_indices_it != deselected_indices.end();
-       ++deselected_indices_it) {
-    QTreeWidgetItem* item = itemFromIndex(*deselected_indices_it);
-    RenderWindowItem* render_window_item = dynamic_cast<RenderWindowItem*>(item);
+  for (const auto& deselected_index : deselected_indices) {
+    QTreeWidgetItem* item = itemFromIndex(deselected_index);
+    auto* render_window_item = dynamic_cast<RenderWindowItem*>(item);
     if (render_window_item != nullptr) {
       render_window_item->getRenderWindow()->setActive(false);
     }

apps/src/openni_grab_images.cpp
@@ -377,12 +377,9 @@ main(int argc, char** argv)
                     << "Supported image modes for device: " << device->getVendorName()
                     << " , " << device->getProductName() << std::endl;
           modes = grabber.getAvailableImageModes();
-          for (std::vector<std::pair<int, XnMapOutputMode>>::const_iterator it =
-                   modes.begin();
-               it != modes.end();
-               ++it) {
-            std::cout << it->first << " = " << it->second.nXRes << " x "
-                      << it->second.nYRes << " @ " << it->second.nFPS << std::endl;
+          for (const auto& mode : modes) {
+            std::cout << mode.first << " = " << mode.second.nXRes << " x "
+                      << mode.second.nYRes << " @ " << mode.second.nFPS << std::endl;
           }
         }
       }

commit 35c572f4d63aa40d3db87a31e7758b56e36853d3

Fix wrong comparison in apps Introduced in https://github.com/PointCloudLibrary/pcl/commit/dce3680b6b8acf665986d1449f7459388e5d7922

apps/3d_rec_framework/include/pcl/apps/3d_rec_framework/pc_source/source.h
@@ -196,7 +196,7 @@ public:
   {
     models_->erase(std::remove_if(models_->begin(),
                                   models_->end(),
-                                  [=](ModelT& s) { return (s.id_ == model_id); }),
+                                  [=](ModelT& s) { return (s.id_ != model_id); }),
                    models_->end());
 
     return models_;